d23a80ebae0f615c5c235865bac273728b618e45
Kundan <kundanss22@gmail.com>
2025-10-30 21:21:16 +0530

    project setup along with tailwind

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <p className='text-4xl text-center '> Hello Sir!!!</p>
+      </div>
+    </>
+  )
+}
+
+export default App
